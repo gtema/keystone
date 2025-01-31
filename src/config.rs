f6e2f16deb49ba0774af78309d4a2b6d2d6f93dd
Artem Goncharov <artem.goncharov@gmail.com>
2025-01-31 19:42:58 +0100

    Cover user list/show
    - Add license headers
    - add readme

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,76 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 use config::{File, FileFormat};
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 #[derive(Debug, Default, Deserialize, Clone)]
 pub struct Config {
+    /// Global configuration options
     #[serde(rename = "DEFAULT")]
     pub default: Option<DefaultSection>,
 
+    /// Database configuration
+    pub database: DatabaseSection,
+
+    /// Identity provider related configuration
     pub identity: Option<IdentitySection>,
+
+    /// Security compliance
+    #[serde(default)]
+    pub security_compliance: SecurityComplianceSection,
+
+    /// User options id to name mapping
+    #[serde(default = "default_user_options_mapping")]
+    pub user_options_id_name_mapping: HashMap<String, String>,
 }
 
 #[derive(Debug, Default, Deserialize, Clone)]
 pub struct DefaultSection {}
 
 #[derive(Debug, Default, Deserialize, Clone)]
+pub struct DatabaseSection {
+    pub connection: String,
+}
+
+#[derive(Debug, Default, Deserialize, Clone)]
 pub struct IdentitySection {
-    pub driver: Option<String>,
+    #[serde(default = "default_identity_driver")]
+    pub driver: String,
+}
+
+#[derive(Debug, Default, Deserialize, Clone)]
+pub struct SecurityComplianceSection {
+    pub password_expires_days: Option<u64>,
+}
+
+fn default_identity_driver() -> String {
+    "sql".into()
+}
+
+fn default_user_options_mapping() -> HashMap<String, String> {
+    HashMap::from([
+        (
+            "1000".into(),
+            "ignore_change_password_upon_first_use".into(),
+        ),
+        ("1001".into(), "ignore_password_expiry".into()),
+        ("1002".into(), "ignore_lockout_failure_attempts".into()),
+        ("1003".into(), "lock_password".into()),
+    ])
 }
 
 impl Config {
--- src/identity/types.rs
+++ src/identity/types.rs
@@ -1,23 +1,78 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 use async_trait::async_trait;
+use chrono::{DateTime, Utc};
+use derive_builder::Builder;
 use sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+use crate::identity::IdentityProviderError;
+
+#[derive(Builder, Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct User {
     pub id: String,
     pub domain_id: String,
-    pub name: Option<String>,
+    pub name: String,
+    pub enabled: bool,
+    #[builder(setter(into, strip_option), default)]
+    pub extra: Option<Value>,
+    #[builder(setter(into, strip_option), default)]
+    pub password_expires_at: Option<DateTime<Utc>>,
+    #[builder(setter(into), default)]
+    pub options: UserOptions,
+}
+
+impl UserBuilder {
+    pub fn get_options(&self) -> Option<&UserOptions> {
+        self.options.as_ref()
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
+pub struct UserOptions {
+    pub ignore_change_password_upon_first_use: Option<bool>,
+    pub ignore_password_expiry: Option<bool>,
+    pub ignore_lockout_failure_attempts: Option<bool>,
+    pub lock_password: Option<bool>,
+    pub ignore_user_inactivity: Option<bool>,
+    pub multi_factor_auth_rules: Option<Vec<Vec<String>>>,
+    pub multi_factor_auth_enabled: Option<bool>,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
-pub struct UserListParameters {}
-
-#[async_trait]
-pub trait IdentityBackend: Send + Sync {
-    async fn list(&self, db: &DatabaseConnection, params: &UserListParameters) -> Vec<User>;
+pub struct UserListParameters {
+    /// Filter users by the domain
+    pub domain_id: Option<String>,
+    /// Filter users by the name attribute
+    pub name: Option<String>,
 }
 
-//#[async_trait]
-//pub trait IdentityUserSrv {
-//    async fn list_users(self, params: &types::UserListParameters) -> Vec<types::User>;
-//}
+#[async_trait]
+pub trait IdentityBackend: Send + Sync + std::fmt::Debug {
+    /// List Users
+    async fn list(
+        &self,
+        db: &DatabaseConnection,
+        params: &UserListParameters,
+    ) -> Result<Vec<User>, IdentityProviderError>;
+
+    /// Get single user
+    async fn get(
+        &self,
+        db: &DatabaseConnection,
+        user_id: String,
+    ) -> Result<Option<User>, IdentityProviderError>;
+}
