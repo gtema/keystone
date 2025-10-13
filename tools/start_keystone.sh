5e91023efbe45bef8fc027e3b2f140f5a07dbd64
Artem Goncharov <Artem.goncharov@gmail.com>
2025-10-13 18:40:22 +0200

    chore: Update py-keystone start script (#304)

--- tools/start_keystone.sh
+++ tools/start_keystone.sh
@@ -4,6 +4,6 @@ keystone-manage db_sync
 
 keystone-manage fernet_setup --keystone-user root --keystone-group root
 
-keystone-manage bootstrap --bootstrap-user admin --bootstrap-password password --bootstrap-public-url http://localhost:15001 --bootstrap-internal-url http://localhost:18080
+keystone-manage bootstrap --bootstrap-user admin --bootstrap-password password --bootstrap-public-url http://localhost:5001 --bootstrap-internal-url http://localhost:8080 --bootstrap-region-id dev
 
-uwsgi --http-socket :5000 --module 'keystone.server.wsgi:initialize_public_application()' -b 65535
+uwsgi --module "keystone.server.wsgi:initialize_public_application()" --http-socket :5001 -b 65535 --http-keepalive --so-keepalive --logformat "Request %(uri):%(method) returned %(status) in %(msecs)ms"
