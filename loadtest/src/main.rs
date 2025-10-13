188008c7ef3a4e7d20c42b079417f0191a02026e
Artem Goncharov <artem.goncharov@gmail.com>
2025-10-13 15:17:54 +0000

    chore: Move loadtest to the functional workflow

--- loadtest/src/main.rs
+++ loadtest/src/main.rs
@@ -1,12 +1,37 @@
-use std::env;
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 
 use goose::prelude::*;
+use std::env;
+
+use openstack_sdk::{AsyncOpenStack, config::ConfigFile};
+
+mod v3;
+
+use crate::v3::users::list as user_list;
+
+struct Session {
+    token: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), GooseError> {
     GooseAttack::initialize()?
         .register_scenario(
-            scenario!("LoadtestTransactions").register_transaction(transaction!(loadtest_index)),
+            scenario!("ListUsers")
+                .register_transaction(transaction!(openstack_login).set_on_start())
+                .register_transaction(transaction!(user_list)),
         )
         .execute()
         .await?;
@@ -14,25 +39,21 @@ async fn main() -> Result<(), GooseError> {
     Ok(())
 }
 
-async fn loadtest_index(user: &mut GooseUser) -> TransactionResult {
-    let token = match env::var("TOKEN") {
-        Ok(token) => token,
-        Err(_) => "foo".to_string(),
-    };
-
-    // Create a Reqwest RequestBuilder object and configure bearer authentication when making
-    // a GET request for the index.
-    let reqwest_request_builder = user
-        .get_request_builder(&GooseMethod::Get, "/v3/users")?
-        .header("x-auth-token", token);
-
-    // Add the manually created RequestBuilder and build a GooseRequest object.
-    let goose_request = GooseRequest::builder()
-        .set_request_builder(reqwest_request_builder)
-        .build();
-
-    // Make the actual request.
-    user.request(goose_request).await?;
+/// Login to OpenStack ($OS_CLOUD) and save the token in the session.
+async fn openstack_login(user: &mut GooseUser) -> TransactionResult {
+    let cfg = ConfigFile::new().unwrap();
+    // Get connection config from clouds.yaml/secure.yaml
+    let profile = cfg
+        .get_cloud_config(env::var("OS_CLOUD").ok().unwrap_or("devstack".to_string()))
+        .unwrap()
+        .unwrap();
+    // Establish connection
+    let session = AsyncOpenStack::new(&profile)
+        .await
+        .expect("cannot connect to the cloud");
+    if let Some(token) = session.get_auth_token() {
+        user.set_session_data(Session { token: token });
+    }
 
     Ok(())
 }
