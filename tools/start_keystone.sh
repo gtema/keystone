0e4df27c66311a8a912568681c36d715454d8c01
Artem Goncharov <Artem.goncharov@gmail.com>
2025-10-24 13:29:27 +0200

    chore: Switch tests to use py-keystone container (#305)

--- tools/start_keystone.sh
+++ tools/start_keystone.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 keystone-manage db_sync
 
@@ -6,4 +7,4 @@ keystone-manage fernet_setup --keystone-user root --keystone-group root
 
 keystone-manage bootstrap --bootstrap-user admin --bootstrap-password password --bootstrap-public-url http://localhost:5001 --bootstrap-internal-url http://localhost:8080 --bootstrap-region-id dev
 
-uwsgi --module "keystone.server.wsgi:initialize_public_application()" --http-socket :5001 -b 65535 --http-keepalive --so-keepalive --logformat "Request %(uri):%(method) returned %(status) in %(msecs)ms"
+exec uwsgi --module "keystone.server.wsgi:initialize_public_application()" --http-socket :5001 -b 65535 --http-keepalive --so-keepalive --logformat "Request %(uri):%(method) returned %(status) in %(msecs)ms"
